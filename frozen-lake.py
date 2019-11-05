1574b5efc1c82e5a4c114c840d6ca2ed7b4387eb
MichaelPascale <32907334+MichaelPascale@users.noreply.github.com>
2019-11-05 16:52:44 -0500

    deeplizard example

--- frozen-lake.py
+++ frozen-lake.py
@@ -34,6 +34,50 @@ d_rate = 0.99 # gamma
 e_rate = 1 # epsilon
 max_e_rate = 1
 min_e_rate = 0.01
-e_decay_rate = 0.01
+e_decay_rate = 0.001
+
+# from deeplizard video https://youtu.be/HGeI30uATws
+# This implementation isn't too bad, but I'd prefer if we made it recursive. Thoughts?
+all_rewards = []
+# Q-Learning
+for episode in range(episodes):
+    # Reset the environment.
+    state = env.reset()
+
+    rewards = 0
+    # time step in each episode
+    for step in range(steps):
+        e_rate_threshold = random.uniform(0, 1)
+        if e_rate_threshold > e_rate:
+            action = numpy.argmax(qtable[state, :]) # What does : mean in Python?
+        else:
+            action = env.action_space.sample()
+
+        new_state, reward, done, info = env.step(action)
+
+        qtable[state, action] = qtable[state, action] * (1 - l_rate) + l_rate * (reward + d_rate * numpy.max(qtable[new_state, :]))
+
+        state = new_state
+        rewards += reward
+
+        if done == True:
+            break
+
+    # episode ends
+    # exploration rate decays
+    min_e_rate + (max_e_rate - min_e_rate) * numpy.exp(-e_decay_rate * episode)
+    all_rewards.append(rewards)
+
+# all episodes done
+# calculate average reward per 1000 episodes
+
+R_per_KE = numpy.split(numpy.array(all_rewards), episodes/1000)
+count = 1000
+for r in R_per_KE:
+    print(count, str(sum(r/1000)))
+    count += 1000
+
+print(qtable)
+
 
 
