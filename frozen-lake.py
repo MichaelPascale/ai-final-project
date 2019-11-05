83093d47dc01612b5ee874bd4d6c748e126b7b66
MichaelPascale <32907334+MichaelPascale@users.noreply.github.com>
2019-11-05 16:03:01 -0500

    testing out gym

--- frozen-lake.py
+++ frozen-lake.py
@@ -11,3 +11,15 @@ env.render()
 
 print("observation, reward, done, info")
 print(env.step(1))
+
+# from deeplizard video
+import numpy
+import gym
+import random
+import time
+a_space_s = env.action_space.n
+s_space_s = env.observation_space.n
+
+# zeros creates a zero filled two dimensional array of Actions x States
+qtable = numpy.zeros((s_space_s, a_space_s))
+print(qtable)
