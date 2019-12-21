adffc24fe04aa174c381f252e2cc0f10ff30796f
MichaelPascale <32907334+MichaelPascale@users.noreply.github.com>
2019-12-20 20:59:31 -0500

    final measurements

--- ql-stats.py
+++ ql-stats.py
@@ -8,9 +8,9 @@ import gym
 import random
 import statistics
 
-sessions = 25
-episodes = 10000
-episodes_to_avg = 100
+sessions = 50
+episodes = 20000
+episodes_to_avg = 1000
 alpha = 0.04180932
 gamma = 0.93332194
 
@@ -18,9 +18,11 @@ e = gym.make('FrozenLake-v0')
 q = qlearner.QLearner(e, episodes, 100)
 
 list_of_rewards = []
-for _ in range(sessions):
-    #list_of_rewards.append(q.run_session(alpha, gamma))
-    list_of_rewards.append(q.run_session(random.random(), random.random()))
+for i in range(sessions):
+    print("Session", i, "of", sessions, flush=True, end='')
+    list_of_rewards.append(q.run_session(alpha, gamma))
+    #list_of_rewards.append(q.run_session(random.random(), random.random()))
+    print("\r", flush=True, end='')
 
 sum_rewards = {}
 for rewards in list_of_rewards:
@@ -28,9 +30,10 @@ for rewards in list_of_rewards:
         sum_rewards[episode] = sum_rewards.get(episode, 0.0) + rewards[episode]
 
 avg_rewards = list(map(lambda x : x/sessions, sum_rewards.values()))
+std_devs = [statistics.stdev(r_tuple) for r_tuple in list(zip(*list_of_rewards))]
 
 print("Sums of the rewards earned over each", episodes_to_avg, "of", episodes, "episodes, rewards averaged over", sessions, "sessions.")
 for i in range(len(avg_rewards)):
     if ((i + 1) % episodes_to_avg is 0) and (i > 0):
-        print(i, sum(avg_rewards[i-episodes_to_avg:i])/episodes_to_avg)
-
+        print(i, sum(avg_rewards[i-episodes_to_avg:i])/episodes_to_avg, sum(std_devs[i-episodes_to_avg:i])/episodes_to_avg)
+        
